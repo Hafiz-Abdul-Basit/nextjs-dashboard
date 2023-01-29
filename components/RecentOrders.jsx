de1f69cf4f21fa75d275b2b5c6287ce4b4a9e71d
Hafiz-Abdul-Basit <110740437+Hafiz-Abdul-Basit@users.noreply.github.com>
2023-01-29 21:39:36 +0500

    my-first-app

---
+++ components/RecentOrders.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { data } from "../data/data";
+import { FaShoppingBag } from "react-icons/fa";
+
+const RecentOrders = () => {
+  return (
+    <div className="w-full col-span-1 relative lg:h-[70vh] h-[50vh] m-auto p-4 mr-4 border rounded-lg bg-white scrollbar-thin scrollbar-thumb-purple-800 scrollbar-corner-purple-200-100">
+      <h1>Recent Orders</h1>
+      <ul>
+        {data.map((order, id) => (
+          <li
+            key={id}
+            className="bg-gray-50 hover:bg-purple-200 rounded-lg my-3 p-3 flex items-center cursor-pointer"
+          >
+            <div className="bg-gray-200 rounded-lg p-3">
+              <FaShoppingBag className="text-purple-800" />
+            </div>
+            <div className="pl-4">
+              <p className="text-gray-800 font-bold">${order.total}</p>
+              <p className="text-gray-400 text-sm">{order.name.first}</p>
+            </div>
+            <p className="lg:flex md:hidden absolute right-12 text-sm">
+              {order.date}
+            </p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default RecentOrders;
